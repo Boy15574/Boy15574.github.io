4265f8b78151e0c2f8cf35b2b13b0c7e7617b3fa
Boy15574 <136392532@qq.com>
2024-07-11 18:58:04 +0800

    Site updated: 2024-07-11 18:58:04

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720695436384";
+const VERSION = "1720695483609";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
